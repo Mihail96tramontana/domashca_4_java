8a7513abd9100f2b7ef7d5388c51f7725d0053a5
parnishca <xelysqgbhju@gmail.com>
2022-06-02 18:23:07 +0300

    test

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -3,18 +3,25 @@ package qa.guru;
 public class Main {
     public static void main(String[] args) {
 
-        //математические и логические операторы
+        //математические и логические операторы (записывать переменные необязазельно)
         System.out.println(5 + 20);
         System.out.println(10 - 5);
         System.out.println(6 >= 12);
         System.out.println(10 != 11);
+        System.out.println(3 < 2);
+        System.out.println(3 <= 2);
+        System.out.println(3 != 2);
+        System.out.println(3 > 2);
 
         //операции переполнения
-        byte qbyte = 127 + 1;
-        System.out.println(qbyte);
+        //byte qbyte = 127 + 1;
+        //System.out.println(qbyte);
 
-        int qint = 2147483647 * 13;
-        System.out.println(qint);
+        //short qshort = 34_000_000;
+
+        //примеры операций над разными типами данных
+
+
 
 
     }
