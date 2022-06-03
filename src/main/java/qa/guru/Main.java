03adc334207bc6bfee9f5a843dbb83d535a3a28c
parnishca <xelysqgbhju@gmail.com>
2022-06-03 17:26:55 +0300

    test23

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println(result);
 
         //вычисления с разными типами данных
-        byte a = 10;
+        byte a = 0;
         int b = 5;
         long c = 100;
         short d = 250;
@@ -26,19 +26,14 @@ public class Main {
         System.out.println("d / c = " + (d / c));
         System.out.println("e * b = " + (e * b));
 
-        float f = 4.24f;
-        int g = 5;
-        System.out.println("Большее число из 2 чисел: " + Math.max(f,g));
-        System.out.println("Меньшее число из 2 чисел: " + Math.min(f,g));
-
         //операции переполнения
         //byte qbyte = 127 + 1; //переполнение
         //System.out.println(qbyte);
 
         //short qshort = 34_000_000; //переполнение
 
-        //вычисления комбинаций типов данных (int и double)
-        System.out.println("Сложение:" + 253 + 232.4);
-        System.out.println("Вычитание:" + 211.85 + 232);
+        //переполнение в min и max сторону
+        System.out.println("В максимальную сторону:" + (Byte.MAX_VALUE + a));
+        System.out.println("В минимальную сторону:" + (Byte.MIN_VALUE - a));
     }
 }
